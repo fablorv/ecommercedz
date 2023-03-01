2ba64fcd20d21cb308cc440c9272ce19eba1bb04
fablorv <fablorvdz@gmail.com>
2023-03-01 03:54:44 +0100

    the product page parts are ready

--- src/pages/products.js
+++ src/pages/products.js
@@ -7,16 +7,27 @@ export default function Products(){
         <button><Link href='/'><img src='#' alt='logo'/></Link></button>
         <button> about us </button>
         <button> PRODUCTS </button>
+        
+        <div> <button> cart </button><button> <Link href='/profile/overview'>profile</Link> </button> </div>
 
       </header>
       <div className="content">
+        <div className='search'>
+          <input type='text' placeholder='what you looking for' />
+          <button> search </button>
+        </div>
         <div className="filter-products">
-            <div className="filter"> filter what u want</div>
+            <div className="filter">
+              filter what u want
+              <div className="size"> choose sizes </div>
+              <div className="color"> choose color </div>
+              <div className="condition"> choose condition </div>
+            </div>
             <div className="products">
               
-              <div className='product1'> here is product1</div>
-              <div className='product2'> here is product2</div>
-              <div className='product3'> here is product3</div>
+              <div className='product1'><p>name of product 1 </p> <p> descreption of the product </p> <p> price of the produuct </p> <button> add to cart </button></div>
+              <div className='product2'><p>name of product 3 </p> <p> descreption of the product </p> <p> price of the produuct </p> <button> add to cart </button></div>
+              <div className='product3'><p>name of product 2 </p> <p> descreption of the product </p> <p> price of the produuct </p> <button> add to cart </button></div>
             </div>
         </div>
       </div>
---
+++ src/pages/profile/overview.js
@@ -0,0 +1,7 @@
+export default function overview(){
+  return(
+    <>
+      overview of aprofile
+    </>
+  )
+}
