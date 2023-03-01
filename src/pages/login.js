9e9ca7d5f4bb80e77956720dc78621f18e75b41e
fablorv <fablorvdz@gmail.com>
2023-03-01 05:39:44 +0100

    everything set up to start making it reactive with logic

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,13 +1,15 @@
-
+import {useState} from 'react'
 
 export default function login(){
+  
+  const [log, setLog] = useState(false)
   return(
 
     <>
-      {true ?<>
+      {log ?<>
 	<input type='text' placeholder='whats your email'/>
-	<input type='password' placeholder='whats ur password' />
-	<button>log in </button> </>
+	<input type='password' placeholder='whats ur password' /> <button> log in </button>
+	</>
 	    :
 	<>
 	  <input type='text' placeholder='username'/>
@@ -15,9 +17,12 @@ export default function login(){
 	  <input type='text' placeholder='family name'/>
 	  <input type='text' placeholder='email'/>
 	  <input type='password' placeholder='password'/>
-	  <input type='password' placeholder='password again'/>
-	  <button> sign up </button> </>
+	  <input type='password' placeholder='password again'/> <button> sign up </button>
+	  </>
       }
+      {!log ? 
+       <button onClick={()=> setLog(!log)}>log in </button>:
+       <button  onClick={()=> setLog(!log)}> sign up </button>}
     </>
   )
 }
