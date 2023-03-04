b3f724956cbea7509a152fd7fcd4468ff4c51eb0
fablorv <fablorvdz@gmail.com>
2023-03-04 17:41:24 +0100

    user adding to cart get saved thro the session

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,6 +1,28 @@
+import {useEffect, useState} from 'react'
+
 export default function cart(){
+  const [localitems , setlocalitems] = useState()
+  useEffect(()=>{
+    let items = localStorage.getItem('arr')
+    if(items){
+
+     let values= JSON.parse(items)
+      console.log(items)
+      setlocalitems(values)
+      console.log(localitems)
+    }else console.log('nothing yet ')
+
+  },[])
+    const testing = ()=>{
+      if(localitems){
+        console.log('the value' ,localitems.current, 'the type:' , typeof localitems)
+      }
+    }
+    testing()
   return(
     <>
+      
+      {localitems ? localitems.current.map((item)=> <> item </>) : <>nothing </>} 
       <p>item 1 u wanna buy</p>
       <button> close </button>
       <p>item 1 u wanna buy</p>
--- src/pages/products.js
+++ src/pages/products.js
@@ -5,10 +5,26 @@ export default function Products(){
   const [itemone, setItemone] = useState(false)
   const [itemtwo, setItemtwo] = useState(false)
   const [itemthree, setItemthree] = useState(false)
+  const [storageO , setstorageO] = useState([])
   let arr2 = useRef([])
     console.log(typeof arr2)
-    console.log('at the end', arr2, 'conditions', itemone, itemtwo, itemthree)
+    console.log('at the end', Object.values(arr2).length , 'conditions', itemone, itemtwo, itemthree)
+  const turnto = (x)=>{
+    x(true)
+    let stringify = JSON.stringify(arr2)
+    localStorage.setItem( 'arr', stringify)
+  }
 
+  useEffect(()=>{
+      if(localStorage.getItem('arr') || Object.values(arr2).length >1 ){
+        console.log(localStorage.getItem('arr'))
+        let values = localStorage.getItem('arr')
+        let cleanvalues = JSON.parse(values)
+        setstorageO(cleanvalues.current)
+      }else console.log('empty')
+  }, [itemone,itemtwo, itemthree ])
+  console.log(storageO, 'test')
+ 
   return(
     <>
       <header>
@@ -33,9 +49,9 @@ export default function Products(){
             </div>
             <div className="products">
               
-              <div className='product1'><p>name of product 1 </p> <p> descreption of the product </p> <p> price of the produuct </p> <button onClick={()=>{ !arr2.current.includes('1') && arr2.current.push('1');setItemone(!itemone)}}>{itemone ? <p>item in cart</p> : <p>add to cart</p>} </button></div>
-              <div className='product2'><p>name of product 3 </p> <p> descreption of the product </p> <p> price of the produuct </p> <button onClick={()=> {!arr2.current.includes('2') && arr2.current.push('2');setItemtwo(!itemtwo)}}> {itemtwo ? <p>item in cart</p> : <p>add to cart</p>} </button></div>
-              <div className='product3'><p>name of product 2 </p> <p> descreption of the product </p> <p> price of the produuct </p> <button onClick={()=> {!arr2.current.includes('3') && arr2.current.push('3');setItemthree(!itemthree)}}> { itemthree ? <p>item in cart </p>: <p>add to cart</p>}  </button></div>
+              <div className='product1'><p>name of product 1 </p> <p> descreption of the product </p> <p> price of the produuct </p> <button onClick={()=>{ !arr2.current.includes('1') && arr2.current.push('1');turnto(setItemone)}}>{ storageO.includes('1') ? <p>item in cart</p> : <p>add to cart</p>} </button></div>
+              <div className='product2'><p>name of product 3 </p> <p> descreption of the product </p> <p> price of the produuct </p> <button onClick={()=> {!arr2.current.includes('2') && arr2.current.push('2');turnto(setItemtwo);}}> {storageO.includes('2')? <p>item in cart</p> : <p>add to cart</p>} </button></div>
+              <div className='product3'><p>name of product 2 </p> <p> descreption of the product </p> <p> price of the produuct </p> <button onClick={()=> {!arr2.current.includes('3') && arr2.current.push('3');turnto(setItemthree)}}> { storageO.includes('3')? <p>item in cart </p>: <p>add to cart</p>}  </button></div>
             </div>
         </div>
       </div>
